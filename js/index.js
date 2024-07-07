d88eb664c6579428dfc9449ddedc338fb11f55fb
tmkvch18 <tomkovich18@gmail.com>
2024-07-08 00:38:43 +0300

    feat: add footer

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function duplicateCount(text) {
+  return text.toLowerCase().split('').filter(function(val, i, arr) {
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
